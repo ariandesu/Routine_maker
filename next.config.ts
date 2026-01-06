1d468ca34352b116a7866982bd93f8a7c6dc4bc5
Mahir Faisal Arian <mahirfaisalarian@gmail.com>
2026-01-06 14:14:46 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -30,6 +31,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  devServer: {
+    allowedDevOrigins: [
+      'https://*.cloudworkstations.dev',
+    ]
+  }
 };
 
 export default nextConfig;
